049cd94cf35296c9984306eebc58423005653a7c
MiguelTFD <MiguelTFD@outlook.com>
2025-10-24 21:59:38 -0500

    genesis

---
+++ src/main/java/com/cakecandy/CakecandyConfigServerApplication.java
@@ -0,0 +1,13 @@
+package com.cakecandy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CakecandyConfigServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CakecandyConfigServerApplication.class, args);
+	}
+
+}
